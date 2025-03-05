611581a1851831ce50f9a94ad48d89346b2f67b6
joseybarranco <joseyenred@hotmail.com>
2025-03-05 12:49:21 +0100

    He puesto un elemento en landing
    He colocado un elemento decorativo en el footer de landing

--- src/views/ahorcado2.vue
+++ src/views/ahorcado2.vue
@@ -1,62 +1,61 @@
 <template>
-    <div>
-      <input v-model="letra" @keyup.enter="ingresarLetra" />
-      <button @click="ingresarLetra">Ingresar letra</button>
-      <p>Palabra: {{ palabra }}</p>
-      <p>Intentos restantes: {{ intentosRestantes }}</p>
-      <div v-html="dibujoAhorcado"></div>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        palabra: "_ _ _ _ _ _",
-        letra: "",
-        intentosRestantes: 6,
-        dibujoAhorcado: "",
-      };
+  <div>
+    <input v-model="letra" @keyup.enter="ingresarLetra" />
+    <button @click="ingresarLetra">Ingresar letra</button>
+    <p>Palabra: {{ palabra }}</p>
+    <p>Intentos restantes: {{ intentosRestantes }}</p>
+    <div v-html="dibujoAhorcado"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      palabra: "_ _ _ _ _ _",
+      letra: "",
+      intentosRestantes: 6,
+      dibujoAhorcado: "",
+    };
+  },
+  methods: {
+    ingresarLetra() {
+      // Comprobar si la letra es correcta
+      if (this.palabra.includes(this.letra)) {
+        // Actualizar la palabra
+        this.palabra = this.palabra.replace("_", this.letra);
+      } else {
+        // Disminuir el número de intentos restantes
+        this.intentosRestantes--;
+        // Dibujar el ahorcado
+        this.dibujarAhorcado();
+      }
+      // Limpiar el input
+      this.letra = "";
     },
-    methods: {
-      ingresarLetra() {
-        // Comprobar si la letra es correcta
-        if (this.palabra.includes(this.letra)) {
-          // Actualizar la palabra
-          this.palabra = this.palabra.replace("_", this.letra);
-        } else {
-          // Disminuir el número de intentos restantes
-          this.intentosRestantes--;
-          // Dibujar el ahorcado
-          this.dibujarAhorcado();
-        }
-        // Limpiar el input
-        this.letra = "";
-      },
-      dibujarAhorcado() {
-        // Dibujar el ahorcado según el número de intentos restantes
-        switch (this.intentosRestantes) {
-          case 5:
-            this.dibujoAhorcado = "<div>_______</div>";
-            break;
-          case 4:
-            this.dibujoAhorcado = "<div>_______</div><div>|</div>";
-            break;
-          case 3:
-            this.dibujoAhorcado = "<div>_______</div><div>|</div><div>/</div>";
-            break;
-          case 2:
-            this.dibujoAhorcado = "<div>_______</div><div>|</div><div>/</div><div>\\</div>";
-            break;
-          case 1:
-            this.dibujoAhorcado = "<div>_______</div><div>|</div><div>/</div><div>\\</div><div>|</div>";
-            break;
-          default:
-            this.dibujoAhorcado = "<div>_______</div><div>|</div><div>/</div><div>\\</div><div>|</div><div>_____</div>";
-            break;
-        }
-      },
+    dibujarAhorcado() {
+      // Dibujar el ahorcado según el número de intentos restantes
+      switch (this.intentosRestantes) {
+        case 5:
+          this.dibujoAhorcado = "<div>_______</div>";
+          break;
+        case 4:
+          this.dibujoAhorcado = "<div>_______</div><div>|</div>";
+          break;
+        case 3:
+          this.dibujoAhorcado = "<div>_______</div><div>|</div><div>/</div>";
+          break;
+        case 2:
+          this.dibujoAhorcado = "<div>_______</div><div>|</div><div>/</div><div>\\</div>";
+          break;
+        case 1:
+          this.dibujoAhorcado = "<div>_______</div><div>|</div><div>/</div><div>\\</div><div>|</div>";
+          break;
+        default:
+          this.dibujoAhorcado = "<div>_______</div><div>|</div><div>/</div><div>\\</div><div>|</div><div>_____</div>";
+          break;
+      }
     },
-  };
-  </script>
-  
+  },
+};
+</script>
--- src/views/landing.vue
+++ src/views/landing.vue
@@ -24,6 +24,13 @@
             </div>
           
         </div>
+        
+      </div>
+      <div class="juegos">
+        <span class="juego juego1" :style="{ left: juego1Left + 'px' }">Ahorcado v1</span>
+        <span class="juego juego2" :style="{ left: juego2Left + 'px' }">Ahorcado v2</span>
+        <span class="juego juego3" :style="{ left: juego3Left + 'px' }">Poker Planning</span>
+        <span class="juego juego4" :style="{ left: juego4Left + 'px' }">Ruleta de la Suerte</span>
       </div>
     </div>
   </template>
@@ -72,6 +79,11 @@
             
           },
         ],
+        juego1Left: 0,
+        juego2Left: 300,
+        juego3Left: 600,
+        juego4Left: 900,
+        velocidad: 2,
       };
     },
     methods: {
@@ -80,6 +92,27 @@
   
         this.$router.push(id);
       },
+    },
+    mounted() {
+      setInterval(() => {
+        this.juego1Left += this.velocidad;
+        this.juego2Left += this.velocidad;
+        this.juego3Left += this.velocidad;
+        this.juego4Left += this.velocidad;
+
+        if (this.juego1Left > window.innerWidth -200) {
+          this.juego1Left = -200;
+        }
+        if (this.juego2Left > window.innerWidth -200) {
+          this.juego2Left = -200;
+        }
+        if (this.juego3Left > window.innerWidth -200) {
+          this.juego3Left = -200;
+        }
+        if (this.juego4Left > window.innerWidth -200) {
+          this.juego4Left = -200;
+        }
+      }, 16);
     },
   };
   </script>
@@ -108,7 +141,7 @@
     display: flex;
     grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
     gap: 20px;
-    margin-top: 15%;
+    margin-top: 10%;
     width: 80%;
     margin-left:10%;
     justify-content: space-around;
@@ -177,4 +210,83 @@
     text-align: center;
     
   }
+
+  .juegos {
+  position: relative;
+  border:5px solid white;
+  border-radius: 10px;
+  padding:35px;
+  overflow: hidden;
+ 
+  margin-top:5%;
+}
+
+.juego {
+ 
+  position: absolute;
+  padding: 10px;
+  border: 2px solid;
+  border-radius: 10px;
+  font-size: 24px;
+  font-weight: bold;
+  transition: top 0.5s, left 0.5s;
+  top: 50%;
+  transform: translateY(-50%);
+  font-size: 24px;
+  font-weight: bold;
+}
+
+.juego:hover {
+ 
+  color: #000; /* Negro */
+  
+}
+
+.juego1 {
+  border-color: #ff0000;
+}
+
+.juego1:hover {
+  background-color: #ff0000;
+}
+.juego2 {
+  border-color: #00ff00;
+}
+.juego2:hover{
+  background-color: #00ff00;
+}
+
+.juego3 {
+  border-color: #0000ff;
+}
+
+.juego3:hover {
+  background-color: #0000ff;
+}
+
+.juego4 {
+  border-color: #ffff00;
+}
+
+.juego4:hover {
+  background-color: #ffff00;
+}
+
+@keyframes mover {
+  0% {
+    transform: translate(0, 0);
+  }
+  25% {
+    transform: translate(100px, 0);
+  }
+  50% {
+    transform: translate(100px, 100px);
+  }
+  75% {
+    transform: translate(0, 100px);
+  }
+  100% {
+    transform: translate(0, 0);
+  }
+}
   </style>
